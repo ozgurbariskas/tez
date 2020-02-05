c548d019bb65847191a4ab8bef5e178adc9918fa
mustafa5555 <44072936+mustafa5555@users.noreply.github.com>
2020-02-05 11:26:56 +0300

    bizeulasduzeltildi

--- screens/BizeUlasScreen.js
+++ screens/BizeUlasScreen.js
@@ -5,7 +5,7 @@ export default class BizeUlasScreen extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Bize ukklas</Text>
+                <Text>Bize ulas</Text>
             </View>
         );
     };
